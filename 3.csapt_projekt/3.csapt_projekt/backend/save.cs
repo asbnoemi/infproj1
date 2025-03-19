833196836dcb9e71949c21f8df5386784d2df996
asbnoemi <asboth.nono@gmail.com>
2025-03-19 12:09:55 +0100

    mentés

---
+++ 3.csapt_projekt/3.csapt_projekt/backend/save.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.csapt_projekt.backend
+{
+    internal class save
+    {
+        public short timeMinut;//játékidö perc ben
+        public short mep;//pája sorszáma
+        public short score;//öszegyüjtött pontok
+        public byte[] top3;//3 legjobb eredmény
+    }
+   
+
+}
