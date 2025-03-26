53dcd7108609b905cb84273e0b7bf956c10bcafb
asbnoemi <asboth.nono@gmail.com>
2025-03-26 20:30:50 +0100

    s

--- 3.csapt_projekt/3.csapt_projekt/backend/shave.cs
+++ 3.csapt_projekt/3.csapt_projekt/backend/shave.cs
@@ -13,8 +13,9 @@ namespace _3.csapt_projekt.backend
     public class Shave //mentés fájl ját kezelö osztály
 
     {
+        const string filename = @"c:\temp\shave.json";//proba bekek még alitani
         private static Shave _me; //hivatkozás Shave.me.ScoreLs.lista fügvény
-        public static Shave me { get {
+        public static Shave Me { get {
             if (_me==null) _me=new Shave(); return _me;
             } }
         public class ScoreC
@@ -23,7 +24,7 @@ namespace _3.csapt_projekt.backend
             public short Mep { get; set; }//pája sorszáma
             public short Score { get; set; }//öszegyüjtött pontok
         }
-        public List<ScoreC> scoreLs { get; set; }
+        public List<ScoreC> scoreLs { get; set; }=new List<ScoreC>();
        //public short[] Top3 { get; set; }//3 legjobb eredmény
       
 
@@ -51,15 +52,15 @@ namespace _3.csapt_projekt.backend
                  Top3[1] = top3[1];
              }
          }*/
-        private Shave() { }
-        public static bool ShaveFile( string svFile = "saves.json")
+        public Shave() { }
+        public bool ShaveFile( string svFile =filename)
         {
 
             try
             {
                 
                string json= JsonSerializer.Serialize<Shave>(_me );
-                
+                File.WriteAllText(svFile, json);
                 return true;
             }
             catch (Exception)
@@ -67,9 +68,9 @@ namespace _3.csapt_projekt.backend
                 return false;
             }
         }
-        public static void load(string svFile) 
+        public void load(string svFile =filename) 
         {
-            Shave shave = JsonSerializer.Deserialize<Shave>(svFile="shaves.json");
+            Shave shave = JsonSerializer.Deserialize<Shave>(File.ReadAllText(svFile));
             _me = shave;
         }
     
