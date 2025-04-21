2f0964e6f0e41fc885761a12917e82ee33511c27
asbnoemi <asboth.nono@gmail.com>
2025-04-22 00:11:16 +0200

    Victori függvény

---
+++ 3.csapt_projekt/3.csapt_projekt/backend/appfunction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.csapt_projekt.backend
+{
+    public static class appfunction
+    {
+
+        public static int Victori(int maxpoint, int maxfault, int actualpoint, int actualfauli)
+        {
+            int actual = 0;//0=nincs vége 1=nyert 2=vesztett
+            if (actualpoint >= maxpoint)
+            {
+                actual = 1; //nyert
+            }
+            else if (actualfauli >= maxfault)
+            {
+                actual = 2; //vesztett
+            }
+            else
+            {
+                actual = 0; //nincs vége
+            }
+
+            return actual;
+        }
+        public static bool ButtonAnable()
+        {
+
+            return false;
+        }
+       
+
+    }
+}
---
+++ 3.csapt_projekt/3.csapt_projektTests1/backend/appfunctionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _3.csapt_projektTests1.backend
+{
+    [TestClass]
+    public class appfunctionTests
+    {
+        [TestMethod]
+        public void Victori()
+        {
+
+            int maxpoint = 5;
+            int maxfault = 3;
+            int actualpoint = 5;
+            int actualfauli = 1;
+            int result = _3.csapt_projekt.backend.appfunction.Victori(maxpoint, maxfault, actualpoint, actualfauli);
+            Assert.AreEqual(1, result); // 1=nyert
+        }
+    }
+    
+}
