bbed0af1a3222d9b1ef64234a25dd0dee55a2372
Hmarkm <hajnal.mark.mihaly@gmail.com>
2025-04-26 15:17:34 +0200

    done

--- 3.csapt_projekt/3.csapt_projekt/Scores.xaml.cs
+++ 3.csapt_projekt/3.csapt_projekt/Scores.xaml.cs
@@ -27,16 +27,27 @@ namespace _3.csapt_projekt
             
             int[] top3index = new int[3] { 0, 0, 0 };
             top3index = Shave.Me.roppscore();
-            int time1 = Shave.Me.scoreLs[top3index[0]].TimeMinut / 60;
-            int time2 = Shave.Me.scoreLs[top3index[1]].TimeMinut / 60;
-            int time3 = Shave.Me.scoreLs[top3index[2]].TimeMinut / 60;
+            TimeSpan ts = TimeSpan.FromSeconds(Shave.Me.scoreLs[top3index[0]].TimeMinut);
+            string time1 = $"{ts.Minutes}:{ts.Seconds:D2}";
+            ts = TimeSpan.FromSeconds(Shave.Me.scoreLs[top3index[1]].TimeMinut);
+            string time2 = $"{ts.Minutes}:{ts.Seconds:D2}";
+            ts = TimeSpan.FromSeconds(Shave.Me.scoreLs[top3index[2]].TimeMinut);
+            string time3 = $"{ts.Minutes}:{ts.Seconds:D2}";
+            //int sec1 = Shave.Me.scoreLs[top3index[0]].TimeMinut / 60;
+            //string time1 = sec1 / 60 + ":" + sec1 % 60;
+            //int sec2 = Shave.Me.scoreLs[top3index[1]].TimeMinut / 60;
+            //string time2 = sec2 / 60 + ":" + sec2 % 60;
+            //int sec3 = Shave.Me.scoreLs[top3index[2]].TimeMinut / 60;
+            //string time3 = sec3 / 60 + ":" + sec3 % 60;
             first.Text = time1.ToString();
             second.Text = time2.ToString();
             third.Text = time3.ToString();
             List<string> ScoreComboBox = new List<string>();
             for (int i = 0; i < Shave.Me.scoreLs.Count; i++)
             {
-                ScoreComboBox.Add("map: " + Shave.Me.scoreLs[i].Mep.ToString() + " pont: " + Shave.Me.scoreLs[i].Score.ToString() + " idő:" + (Shave.Me.scoreLs[i].TimeMinut / 60).ToString());
+                ts = TimeSpan.FromSeconds(Shave.Me.scoreLs[i].TimeMinut);
+                string time = $"{ts.Minutes}:{ts.Seconds:D2}";
+                ScoreComboBox.Add("map: " + Shave.Me.scoreLs[i].Mep.ToString() + " pont: " + Shave.Me.scoreLs[i].Score.ToString() + " idő:" + time);
             }
             ComboBox.ItemsSource = ScoreComboBox;
         }
