e48a5ad416c2d3e2c860426e5850d44172153216
Alexaaaaaaaaaaaaaa <baranyi.alexa04@gmail.com>
2025-04-27 12:12:49 +0200

    Scoreson a top 3nál Scoreok kiírva

--- 3.csapt_projekt/3.csapt_projekt/Scores.xaml.cs
+++ 3.csapt_projekt/3.csapt_projekt/Scores.xaml.cs
@@ -34,9 +34,9 @@ namespace _3.csapt_projekt
             ts = TimeSpan.FromSeconds(Shave.Me.scoreLs[top3index[2]].TimeMinut);
             string time3 = $"{ts.Minutes}:{ts.Seconds:D2}";
 
-            firstScore.Text = (Shave.Me.scoreLs[top3index[0]]).ToString();
-            secondScore.Text = (Shave.Me.scoreLs[top3index[1]]).ToString();
-            thirdScore.Text = (Shave.Me.scoreLs[top3index[2]]).ToString();
+            firstScore.Text = (Shave.Me.scoreLs[top3index[0]].Score).ToString();
+            secondScore.Text = (Shave.Me.scoreLs[top3index[1]].Score).ToString();
+            thirdScore.Text = (Shave.Me.scoreLs[top3index[2]].Score).ToString();
             //int sec1 = Shave.Me.scoreLs[top3index[0]].TimeMinut / 60;
             //string time1 = sec1 / 60 + ":" + sec1 % 60;
             //int sec2 = Shave.Me.scoreLs[top3index[1]].TimeMinut / 60;
